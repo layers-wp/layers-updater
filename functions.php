5f1fbb63be29bed2d1b70b9a48183bca16095d62
Marc Perel <marc@obox.co.za>
2015-01-13 13:52:55 +0200

    Changed updater to use WP Settings API

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php /*
- * Plugin Name: Hatch - Updater
+ * Plugin Name: Layers - Updater
  * Version: 1.0-beta
  * Plugin URI: http://www.oboxthemes.com
- * Description: This plugin adds the Hatch Updater script
+ * Description: This plugin adds the Layers Updater script
  * Author: Marc Perel
  * Author URI: http://www.oboxthemes.com/
  * Requires at least: 4.0
  * Tested up to: 4.0
  *
- * Text Domain: hatch-updater
+ * Text Domain: layer-updater
  * Domain Path: /lang/
  *
  * @package WordPress
@@ -18,33 +18,34 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 if ( defined( 'SCRIPT_DEBUG' ) && TRUE == SCRIPT_DEBUG ) {
-    define( 'HATCH_UPDATER_VER', rand( 0 , 100 ) );
+    define( 'LAYERS_UPDATER_VER', rand( 0 , 100 ) );
 } else {
-    define( 'HATCH_UPDATER_VER', '1.0-beta' );
+    define( 'LAYERS_UPDATER_VER', '1.0-beta' );
 }
 
-define( 'HATCH_UPDATER_SLUG' , 'hatch-updater' );
-define( 'HATCH_UPDATER_DIR' , plugin_dir_path( __FILE__ ) );
-define( 'HATCH_UPDATER_URI' , plugin_dir_url( __FILE__ ) );
+define( 'LAYERS_UPDATER_REMOTE_URL' , 'vagrant.localhost/api/v1' );
+define( 'LAYERS_UPDATER_SLUG' , 'layer-updater' );
+define( 'LAYERS_UPDATER_DIR' , plugin_dir_path( __FILE__ ) );
+define( 'LAYERS_UPDATER_URI' , plugin_dir_url( __FILE__ ) );
 
-if( !function_exists( 'hatch_updater_init' ) ) {
+if( !function_exists( 'layer_updater_init' ) ) {
     // Instantiate Plugin
-    function hatch_updater_init() {
+    function layer_updater_init() {
 
-        // Check for the existance of the Hatch Options Panel class
-        if( !class_exists( 'Hatch_Options_Panel' ) ) return false;
+        // Check for the existance of the Layers Options Panel class
+        if( !class_exists( 'Layers_Options_Panel' ) ) return false;
 
         // Load plugin class files
         $included = require_once( 'includes/class-updater.php' );
 
-        global $hatch_updater;
+        global $layer_updater;
 
-        $hatch_updater = new Hatch_Updater();
-        $hatch_updater->init();
+        $layer_updater = new Layers_Updater();
+        $layer_updater->init();
 
         // Localization
-        load_plugin_textdomain( HATCH_UPDATER_SLUG, FALSE, dirname( plugin_basename( __FILE__ ) ) . "/lang/" );
-    } // hatch_updater_init
+        load_plugin_textdomain( LAYERS_UPDATER_SLUG, FALSE, dirname( plugin_basename( __FILE__ ) ) . "/lang/" );
+    } // layer_updater_init
 
-    add_action( "after_setup_theme", "hatch_updater_init", 100 );
+    add_action( "after_setup_theme", "layer_updater_init", 100 );
 }
--- includes/class-updater.php
+++ includes/class-updater.php
@@ -1,12 +1,14 @@
 <?php  /**
- * Hatch Updater Class
+ * Layers Updater Class
  *
  * This file is used to modify any Updater related filtes, hooks & modifiers
  *
- * @package Hatch
- * @since Hatch 1.0
+ * @package Layers
+ * @since Layers 1.0
  */
-class Hatch_Updater {
+class Layers_Updater {
+
+    const LAYERS_API_REMOTE_URL = 'vagrant.localhost/api/v1';
 
     private static $instance;
 
@@ -26,7 +28,11 @@ class Hatch_Updater {
 
         require_once( 'class-options-panel.php' );
 
-        add_action( 'admin_menu' , array( $this, 'add_menu' ) , 70 );
+        // Add the administrator menu
+        add_action( 'admin_init', array( $this, 'add_settings' ) );
+
+        // Save API key
+        add_action( 'init' , array( $this, 'save_api_key' ) );
 
         // Theme and Plugin Update Checkers
         add_filter( 'pre_set_site_transient_update_themes', array( $this, 'transient_theme_updates' ) );
@@ -42,11 +48,11 @@ class Hatch_Updater {
 
         // Backup Page
         add_submenu_page(
-            HATCH_THEME_SLUG . '-welcome',
-            __( 'Update' , HATCH_THEME_SLUG ),
-            __( 'Update' , HATCH_THEME_SLUG ),
+            LAYERS_UPDATER_SLUG . '-welcome',
+            __( 'Update' , LAYERS_UPDATER_SLUG ),
+            __( 'Update' , LAYERS_UPDATER_SLUG ),
             'manage_options',
-            HATCH_THEME_SLUG . '-register',
+            LAYERS_UPDATER_SLUG . '-register',
             array( $this , 'options_panel_ui' )
         );
 
@@ -55,11 +61,117 @@ class Hatch_Updater {
     /**
     *  Add the Options Panel class
     */
+    public function add_settings() {
+        add_settings_section(
+            'layers_update_section',
+            __( 'Layers Automatic Update Settings', LAYERS_UPDATER_SLUG ),
+            NULL,
+            'general'
+        );
 
-    public function options_panel_ui(){
+        add_settings_field(
+            'layers_api_key',
+            __( 'API Key', LAYERS_UPDATER_SLUG ),
+            array( $this, 'show_api_key_setting' ),
+            'general',
+            'layers_update_section',
+            array( 'label_for' => 'layers_api_key' )
+        );
 
-        $hatch_options_panel = new Hatch_Updater_Options_Panel();
-        $hatch_options_panel->init();
+        register_setting( 'general', 'layers_api_key' );
+    }
+
+    public function show_api_key_setting() {
+        printf(
+            '<input name="layers_api_key" id="layers_api_key" type="text" value="%1$s" class="regular-text" />%2$s',
+            $this->_get_api_key(),
+            __( '<p class="description">Follow this link to get your API credentials <a href="http://oboxthemes.com/api">Obox Themes API</a></p>', LAYERS_UPDATER_SLUG )
+        );
+    }
+
+    /**
+    *  Obox API Call
+    */
+
+    private function _do_api_call( $endpoint = 'verify', $apikey = NULL, $return_array = true ) {
+
+        $api_param = ( NULL != $apikey ? '?apikey=' . $apikey : '' );
+
+        $api_call = wp_remote_get(
+            'http://' . self::LAYERS_API_REMOTE_URL . '/' . $endpoint . $api_param,
+            array(
+                    'timeout' => 60,
+                    'httpversion' => '1.1'
+                )
+        );
+
+        if( is_wp_error( $api_call ) ) return NULL;
+
+        $body_as_array = json_decode( $api_call['body'], $return_array );
+
+        return $body_as_array;
+    }
+
+    /**
+    * Get Obox API key
+    */
+
+    private function _get_api_key(){
+
+        global $layers_api_key;
+
+        if( !isset( $layers_api_key ) ){
+            $layers_api_key = get_option( 'layers_api_key' );
+        }
+
+        return $layers_api_key;
+    }
+
+    /**
+    * Fetch available updates
+    */
+
+    private function _get_available_updates( $type = 'themes' ){
+
+        // Get data
+        $response = $this->_do_api_call( 'updates', $this->_get_api_key(), true );
+
+        // If the response is not successful do nothing, just return; @TODO: Add a messaging system hook in
+        if( NULL == $response ) return;
+
+        // For themes return an array, for plugins return a StdObject
+        if( 'themes' == $type ) {
+            return json_decode( json_encode( $response['data'] ), true );
+        } else {
+            return json_decode( json_encode( $response['data'] ) );
+        }
+    }
+
+    /**
+    *  API key Saving
+    */
+    public function save_api_key(){
+        global $layers_register_message;
+
+        if( isset( $_REQUEST[ '_wpnonce_layers_api_key' ] ) ){
+
+            // Get the posted API key
+            $apikey = $_POST[ 'layers_obox_api_key' ];
+
+            // Get data
+            $apicheck = $this->_do_api_call( 'verify', $apikey, true );
+
+            if( false == $apicheck[ 'success' ] ){
+                $layers_register_message = $apicheck[ 'message' ];
+                return;
+            }
+
+            if( ! wp_verify_nonce( $_REQUEST[ '_wpnonce_layers_api_key' ], 'layers_save_api_key' ) ) return;
+
+            $layers_register_message = __( 'Your API key is valid!' , LAYERS_U);
+
+            update_option( 'layers_api_key' , $apikey );
+        }
     }
 
     /**
@@ -67,13 +179,20 @@ class Hatch_Updater {
     */
 
     public function transient_theme_updates( $theme_data ) {
+        // Get an array of existing themes
+        $existing_themes = wp_get_themes();
 
         // Update API Data
         $data = $this->_get_available_updates();
-        // Loop over plugins looking for the latest version
 
+        // Loop over plugins looking for the latest version
         if ( isset( $data[ 'themes' ] ) ) {
+
             foreach ( $data[ 'themes' ] AS $key => $t ) {
+
+                // Be sure that we have this theme installed
+                if( !array_key_exists( $key, $existing_themes ) ) continue;
+
                 $theme_data->response[ $t['template'] ] = $t[ 'update' ];
             }
         }
@@ -89,13 +208,17 @@ class Hatch_Updater {
 
         foreach( $available_plugins as $slug => $details ){
             $plugin = explode( '/', $slug );
-
             $base = $plugin[0];
-            $file = $plugin[1];
-
-            if( $base == $plugin_slug_to_find ) {
-                $plugin_slug = $base . '/' . $file;
+            if( !isset( $plugin[1] ) ) {
+                $plugin_slug = $file;
                 break;
+            } else {
+                $file = $plugin[1];
+
+                if( $base == $plugin_slug_to_find ) {
+                    $plugin_slug = $base . '/' . $file;
+                    break;
+                }
             }
         }
 
@@ -122,7 +245,13 @@ class Hatch_Updater {
         if ( isset( $data->plugins ) ) {
 
             foreach ( $data->plugins AS $key => $p ) {
+
+                // Get the full plugin slug
                 $plugin_slug = $this->get_plugin_slug( $available_plugins, $p->slug );
+
+                // Make sure that we have this plugin installed
+                if( !$plugin_slug ) continue;
+
                 $plugin_data->response[ $plugin_slug ]->package = $p->update->package;
                 $plugin_data->response[ $plugin_slug ]->new_version = $p->update->new_version;
                 $plugin_data->response[ $plugin_slug ]->slug = $p->update->slug;
@@ -136,42 +265,4 @@ class Hatch_Updater {
 
     } // transient_theme_updates
 
-    private function _get_available_updates( $type = 'themes' ){
-
-        $theme_updates = '{
-            "themes" : {
-                "hatch": {
-                        "template" : "hatch",
-                        "update" : {
-                            "package" : "https://bitbucket.org/marcperel/hatch",
-                            "new_version":  "1.2"
-                        },
-                        "version": "1.1",
-                        "name" : "Hatch"
-                    }
-                }
-            }';
-
-        $plugin_updates = '{
-            "plugins" : {
-                "hatch-woocommerce": {
-                        "slug" : "hatch-woocommerce",
-                        "url" : "http://oboxthemes.com/hatch/extensions/woocommerce",
-                        "update" : {
-                            "slug" : "hatch-woocommerce",
-                            "package" : "https://downloads.wordpress.org/plugin/menu-customizer.0.2.zip",
-                            "new_version":  "1.2"
-                        },
-                        "version": "1.1",
-                        "name" : "Hatch WooCommerce"
-                    }
-                }
-            }';
-
-        if( 'themes' == $type ) {
-            return json_decode( $theme_updates, true );
-        } else {
-            return json_decode( $plugin_updates );
-        }
-    }
 }
